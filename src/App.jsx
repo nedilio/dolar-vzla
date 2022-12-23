7b9811230370a8d76c5cf2b2f4a694bb37020af9
Nelson Izquierdo <izquierdonelson@gmail.com>
2022-12-23 18:48:27 -0300

    feat: agregando dolar BCV

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import { getDolarPrice } from "./services";
 
 function App() {
   const [dolarPrice, setDolarPrice] = useState(0);
+  const [dolarBCV, setDolarBCV] = useState(0);
   const [bolivares, setBolivares] = useState(0);
+  const [bolivaresBCV, setBolivaresBCV] = useState(0);
   const [fecha, setFecha] = useState("");
 
   const handleChange = (dolares) => {
     const numericDolares = dolares.replace(",", ".") * 1;
     setBolivares(Math.ceil(numericDolares * dolarPrice));
+    setBolivaresBCV(Math.ceil(numericDolares * dolarBCV));
   };
 
   const handleClear = () => {
@@ -19,6 +22,7 @@ function App() {
 
   useEffect(() => {
     getDolarPrice().then((res) => {
+      setDolarBCV(res.USD.sicad2);
       setDolarPrice(res.USD.promedio);
       setFecha(res._timestamp.fecha);
     });
@@ -27,7 +31,8 @@ function App() {
   return (
     <div className="App">
       <h1 className="title">Conversión Dolares a Bolívares</h1>
-      <h3>1 USD 🇺🇸 = {dolarPrice} Bs 🇻🇪</h3>
+      <h3>1 USD 🇺🇸 = {dolarPrice} Bs 🇻🇪 (Dolar Today)</h3>
+      <h3>1 USD 🇺🇸 = {dolarBCV} Bs 🇻🇪 (BCV)</h3>
       <p>{fecha}</p>
       <input
         type="text"
@@ -35,7 +40,8 @@ function App() {
         inputmode="decimal"
         onChange={(e) => handleChange(e.target.value)}
       />
-      <h2>Precio en Bolivares: {bolivares}</h2>
+      <h2>Precio en Bolivares Dolar today: {bolivares}</h2>
+      <h2>Precio en Bolivares BCV: {bolivaresBCV}</h2>
       <button disabled={bolivares === 0} onClick={handleClear}>
         Borrar
       </button>
